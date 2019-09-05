b2ab5e29e0621fd59478078cc3eb82f855f2d4e6
Peter Oerholm Nielsen <peter@qrholm.dk>
2019-09-05 21:46:19 +0200

    Adding pictures and a solution for the spoon.

--- asgmt-01/main.cpp
+++ asgmt-01/main.cpp
@@ -1,16 +1,25 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <iostream>
+#include <tuple>
 
 
 
-void rotate(cv::Mat image, int rotation)
+cv::Mat& rotate(cv::Mat &image, cv::Mat &output, int rotation=0)
 {
+    cv::Mat cp_image = image.clone();
     for(int x = 0; x < image.rows ; x++)
     {
-        image.at<cv::Vec3b>(x,0) = 0;
-
+        for(int y = 0; y < image.cols; y++)
+        {
+            for(int channel = 0; channel < image.channels(); channel++)
+            {
+                cp_image.at<cv::Vec3b>(image.rows-1 -y,x)[channel] = image.at<cv::Vec3b>(x,y)[channel];
+            }            
+        }    
     }
+    output = cp_image;
+    return output;
 }
 
 
@@ -25,18 +34,48 @@ void InvertColour( cv::Mat& input_image, cv::Mat& output_image )
  output_image.at<cv::Vec3b>(row,col)[channel] = 255 - input_image.at<cv::Vec3b>(row,col)[channel];
 }
 
+cv::Mat& Segment_spoons(cv::Mat& img,int threshold)
+{
+   for(int x = 0; x < img.rows ; x++)
+    {
+        for(int y = 0; y < img.cols; y++)
+        {
+            if ((img.at<cv::Vec3b>(x,y)[2] < threshold) ||
+               img.at<cv::Vec3b>(x,y)[0] > 30  && img.at<cv::Vec3b>(x,y)[1] > 30
+               )
+            {
+                img.at<cv::Vec3b>(x,y)[0] = 0;
+                img.at<cv::Vec3b>(x,y)[1] = 0;
+                img.at<cv::Vec3b>(x,y)[2] = 0;
+            }
+        }    
+    }
+    return img;
+}
+
 int main()
 {
-    cv::Mat image = cv::imread("assets/Astronaut1.jpg");
+    cv::Mat in_image = cv::imread("assets/BabyFood/BabyFood-Sample1.JPG");
     int test = 0;
-    if (image.empty())
+    if (in_image.empty())
     {
         std::cerr << "Could not load image";
         return 1;
     }
     std::cout<<"Hello world xD "<<test<<std::endl;
     cv::namedWindow("Image");
-    cv::imshow("Image", image);
+    // cv::namedWindow("Image");
+    // cv::Mat out_image;
+    // InvertColour(in_image, out_image);
+    // cv::imshow("Image", in_image);
+    // cv::imshow("Inverted Image", out_image);
+    // rotate(out_image, out_image);
+    // cv::imshow("rotated Image", out_image);
+    // rotate(out_image, out_image);
+    // cv::imshow("rotated in_imag twice", out_image);
+    
+    Segment_spoons(in_image,120);
+    cv::imshow("rotated in_imag twice", in_image);
     cv::waitKey();
     return test;
 
