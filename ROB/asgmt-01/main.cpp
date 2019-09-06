3814ddb192fbba89cb621b187be21c02df493ce5
Peter Nielsen <peter@qrholm.dk>
2019-09-06 14:08:43 +0200

    initial

--- ROB/asgmt-01/main.cpp
+++ ROB/asgmt-01/main.cpp
@@ -10,50 +10,62 @@ struct coords
 
 struct RGB_col
 {
-    int R,G,B;
+    uchar R,G,B;
 };
 
-cv::Mat& insert_goal(cv::Mat& image, coords coordinate, RGB_col color)
+void insert_goal(cv::Mat& image,cv::Mat& output, coords coordinate, RGB_col color)
 {
-    image.at<cv::Vec3b>(coordinate.y, coordinate.x)[2] = color.R;
-    image.at<cv::Vec3b>(coordinate.y, coordinate.x)[1] = color.G;
-    image.at<cv::Vec3b>(coordinate.y, coordinate.x)[0] = color.B;
+    output = image.clone();
+    output.at<cv::Vec3b>(coordinate.y, coordinate.x)[2] = color.R;
+    output.at<cv::Vec3b>(coordinate.y, coordinate.x)[1] = color.G;
+    output.at<cv::Vec3b>(coordinate.y, coordinate.x)[0] = color.B;
+    output.at<cv::Vec3b>(cv::Point2i(coordinate.y, coordinate.x)) = cv::Vec3b{1,2,3};
 }
-cv::Mat& insert_start(cv::Mat&  image, coords coordinate, uchar color)
+void insert_start(cv::Mat&  image, cv::Mat& output, coords coordinate, RGB_col color)
 {
-
+    output = image.clone();
+    output.ptr<RGB_col>(coordinate.y)[coordinate.x] = color;
 }
 
-cv::Mat& draw_path(cv::Mat& image, coords start, coords stop)
+void draw_path(cv::Mat& image, cv::Mat& output, std::vector<coords> data_set)
 {
+    output = image.clone();
+    for(auto& point:data_set)
+    {
+        //output.ptr<RGB_col>(point.y)[]
+
+    }
 
 }
 
 int main()
 {
-    cv::Mat in_image = cv::imread("assets/BabyFood/BabyFood-Sample1.JPG");
-    int test = 0;
-    if (in_image.empty())
+    cv::Mat map = cv::imread("assets/environ-01.png");
+    if (map.empty())
     {
         std::cerr << "Could not load image";
         return 1;
     }
-    std::cout<<"Hello world xD "<<test<<std::endl;
-    cv::namedWindow("Image");
-    // cv::namedWindow("Image");
-    // cv::Mat out_image;
-    // InvertColour(in_image, out_image);
+    cv::Mat new_map = map.clone();
+    
+    //insert_goal(map,new_map, coords{idx,idx}, RGB_col{.R = 128, .G=0, .B=0} );
+    for(int idx = 0; idx < 100; idx++)
+    {
+        //insert_goal(new_map,new_map, coords{idx,idx}, RGB_col{.R = 128, .G=0, .B=0} );
+        insert_start(new_map,new_map, coords{idx,idx}, RGB_col{.R = 0, .G=0, .B=0} );
+    }
+    
+    cv::imshow("rotated in_imag twice", new_map);
     // cv::imshow("Image", in_image);
     // cv::imshow("Inverted Image", out_image);
     // rotate(out_image, out_image);
     // cv::imshow("rotated Image", out_image);
-    rotate(in_image, in_image);
-    cv::imshow("rotated in_imag twice", in_image);
     
-    Segment_spoons(in_image,120);
-    cv::imshow("Segmented Image", in_image);
+    
+    //Segment_spoons(in_image,120);
+    //cv::imshow("Segmented Image", in_image);
     cv::waitKey();
-    return test;
+    return 0;
 
 
 }
