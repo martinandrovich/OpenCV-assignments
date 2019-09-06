3ee26047cb070fb39e0d07153a067ea2b7ff54ef
Martin Androvich <martinandrovich@gmail.com>
2019-09-06 10:28:19 +0200

    Update main.cpp

--- ROB/asgmt-01/main.cpp
+++ ROB/asgmt-01/main.cpp
@@ -1,3 +1,22 @@
+#include <string>
+
+struct coords
+{
+	int x, y;
+};
+
+struct path
+{
+	coords begin;
+	coords end;
+	std::vector<coords> path;
+}
+
+void insert_goal()
+{
+
+}
+
 int main()
 {
 	return 0;
