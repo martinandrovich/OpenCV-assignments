c745153a7f504c5a5b51d77e54c8cc9b8f2a3116
Martin Androvich <martinandrovich@gmail.com>
2019-09-19 15:22:12 +0200

    VIS: Add asgmt-02 and global assets folder

---
+++ VIS/asgmt-02/main.cpp
@@ -0,0 +1,17 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/opencv.hpp"
+
+// forward declarations
+void gauss_noise(cv::Mat& img);
+
+int main()
+{
+	std::cin.get();
+	return 0;
+}
