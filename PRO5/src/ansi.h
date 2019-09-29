fa9040afb762bf4c3addb38bf68cd1c26d6f2c63
Martin Androvich <martinandrovich@gmail.com>
2019-09-29 14:47:42 +0200

    Remove PRO folder

--- PRO5/src/ansi.h
+++
@@ -1,7 +0,0 @@
-namespace ansi
-{
-	constexpr auto reset	= "\033[0m";
-	constexpr auto red		= "\033[31m";
-	constexpr auto green	= "\033[32m";
-	constexpr auto kernel	= "\033[32mkernel:\033[0m ";
-}
